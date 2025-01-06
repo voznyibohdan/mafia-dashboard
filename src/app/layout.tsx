3deb5ce0c0f14d45633d231b8ea683fd55f0e099
BohdanVoznyi <bohdanvoznyi.em@gmail.com>
2025-01-06 12:36:50 +0200

    favicon & cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import '@/styles/index.css';
 
 import { bitter, jetBrains } from '@/shared/fonts';
 
 export const metadata: Metadata = {
-    title: 'Mafia Dashboard',
-    description: 'Dashboard that will help the hosts to conduct the game'
+    title: 'Mafia',
+    description: 'Dashboard that will help the hosts to conduct the game',
+    icons: {
+        icon: '/favicon.svg'
+    }
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
